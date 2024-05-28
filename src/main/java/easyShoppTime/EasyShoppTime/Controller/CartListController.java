994ec939a8e70fa9c4e5ccd5cc58f73001861d48
Luiza <luizagabriella.santos@outlook.com>
2024-05-28 04:50:49 -0300

    front para integração

--- src/main/java/easyShoppTime/EasyShoppTime/Controller/CartListController.java
+++ src/main/java/easyShoppTime/EasyShoppTime/Controller/CartListController.java
@@ -2,19 +2,17 @@ package easyShoppTime.EasyShoppTime.Controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@RequestMapping("/")
 @RestController
 @SpringBootApplication
-public class MyController {
+public class CartListController {
 
-    @RequestMapping("/")
-    String home() {
-    	return "Hello World";
-    }
-    
-    public static void main(String[] args) {
-		SpringApplication.run(MyController.class, args);
-	}
+	 public static void main(String[] args) {
+         SpringApplication.run(CartListController.class, args);
+   }
 }
